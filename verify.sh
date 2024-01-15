55c7d05552c7d92db2f206b5771858b2e5364b1c
Andreas Mosti <andreas.mosti@gmail.com>
2024-01-15 19:31:28 +0100

    Whitelist adidas

--- verify.sh
+++ verify.sh
@@ -1,1 +1,1 @@
-docker run --rm -v $(pwd):/mnt andmos/awesome-bot --allow-redirect --skip-save-results $(find . -path ./themes -prune -o -name "*.md" -print)
+docker run --rm -v $(pwd):/mnt andmos/awesome-bot --allow-redirect --white-list "adidas.no" --skip-save-results $(find . -path ./themes -prune -o -name "*.md" -print)
